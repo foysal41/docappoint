08907272552913c04440e1ffdd55c39d1f7ff6ad
foysal41 <mdfoysal.mf33@example.com>
2026-05-18 01:05:14 +0600

    footer section added

--- src/app/components/global/footer/page.jsx
+++ src/app/components/global/footer/page.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import logo from "@/assets/logo.png";
+import Image from 'next/image';
 
-export default function page() {
+export default function Footer() {
   return (
-    <div>page</div>
+    <div className='w-full mx-auto bg-[#F8F9FD] py-8 px-4'>
+        <div className=' max-w-7xl mx-auto flex justify-between items-center gap-4'>
+            <div>
+                <Image src={logo} alt='logo' height={80} width={150} className='object-cover' ></Image>
+                <p className='text-black/30 text-[14px]'> Book Doctor. Get Better. </p>
+            </div>
+
+             <div>
+                <h2 className='font-bold uppercase  text-[#33B5C5]'>Get In Touch</h2>
+                <p className='text-black/30 text-[14px]'> info@foysaljaman.com</p>
+                <p className='text-black/30 text-[14px]'> +01701035894</p>
+            </div>
+
+              <div>
+                <h2 className='font-bold uppercase  text-[#33B5C5]'>Location</h2>
+                <p className='text-black/30 text-[14px]'> Rupsha Khulna bangladesh</p>
+                <p className='text-black/30 text-[14px]'> Sonadanga, Khulna </p>
+            </div>
+        </div>
+    </div>
   )
 }
